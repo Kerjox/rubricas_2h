d49949d255885102732f5a0e9082d534ed2ed492
kerjox <kerjoxgamer095@gmail.com>
2021-11-08 17:37:44 +0100

    Servlet-base modified to fit knowledge

--- DDI/Bloque3 Rubrica 5 - WebApp Servlet Formulario inscripcion/src/main/java/com/ubalde/bloque3_rubrica_5/HelloServlet.java
+++ DDI/Bloque3 Rubrica 5 - WebApp Servlet Formulario inscripcion/src/main/java/com/ubalde/bloque3_rubrica_5/HelloServlet.java
@@ -2,9 +2,7 @@ package com.ubalde.bloque3_rubrica_5;
 
 import java.io.*;
 import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
 	private String message;
 
