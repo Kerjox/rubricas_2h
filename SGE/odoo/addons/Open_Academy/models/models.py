a8a725ae3978dcff78de1fa179f6a12f76a18336
kerjox <kerjoxgamer095@gmail.com>
2021-11-14 20:20:40 +0100

    Added sessions model

--- SGE/odoo/addons/Open_Academy/models/models.py
+++ SGE/odoo/addons/Open_Academy/models/models.py
@@ -4,3 +4,10 @@ class Course(models.Model):
     _name = 'aca.courses'
     name = fields.Char('Name')
     description = fields.Text('Description')
+
+class Session(models.Model):
+    _name = 'aca.sessions'
+    name = fields.Char('Name')
+    start_date = fields.Date('Start Date')
+    duration = fields.Integer('Duration')
+    seats = fields.Integer('Seats')
