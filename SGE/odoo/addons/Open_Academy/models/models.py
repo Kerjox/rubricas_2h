ada4208019c87a0e4af4441b0190eafb6795fcbd
kerjox <kerjoxgamer095@gmail.com>
2021-11-14 20:53:02 +0100

    Added relationship between sessions and courses

--- SGE/odoo/addons/Open_Academy/models/models.py
+++ SGE/odoo/addons/Open_Academy/models/models.py
@@ -4,6 +4,7 @@ class Course(models.Model):
     _name = 'aca.courses'
     name = fields.Char('Name')
     description = fields.Text('Description')
+    session_id = fields.One2many('aca.sessions', 'course_id', string='Session')
 
 class Session(models.Model):
     _name = 'aca.sessions'
@@ -11,3 +12,4 @@ class Session(models.Model):
     start_date = fields.Date('Start Date')
     duration = fields.Integer('Duration')
     seats = fields.Integer('Seats')
+    course_id = fields.Many2one('aca.courses', string='Course')
