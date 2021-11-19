bb1f8e6fa1237c21ed4b5621bd732398e23932a2
kerjox <kerjoxgamer095@gmail.com>
2021-11-19 09:48:53 +0100

    Modified things on odoo

---
+++ DDI/Practica_Examen_JDBC_ConnPool/src/main/java/com/kerjox/practica_examen_jdbc/connection/ConnectionPool.java
@@ -0,0 +1,91 @@
+package com.kerjox.practica_examen_jdbc.connection;
+
+import org.apache.commons.dbcp.ConnectionFactory;
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.pool.impl.GenericObjectPool;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ConnectionPool {
+
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String JDBC_DB_URL = "jdbc:mysql://0.0.0.0:3306/Alumnos_DDI";
+
+	static final String JDBC_USER = "root";
+	static final String JDBC_PASS = "IVSZ2e12";
+
+	private static GenericObjectPool gPool = null;
+
+	@SuppressWarnings("unused")
+	public DataSource setUpPool() throws Exception {
+		Class.forName(JDBC_DRIVER);
+
+		// Creates an Instance of GenericObjectPool That Holds Our Pool of Connections Object!
+		gPool = new GenericObjectPool();
+		gPool.setMaxActive(5);
+
+		// Creates a ConnectionFactory Object Which Will Be Use by the Pool to Create the Connection Object!
+		ConnectionFactory cf = new DriverManagerConnectionFactory(JDBC_DB_URL, JDBC_USER, JDBC_PASS);
+
+		// Creates a PoolableConnectionFactory That Will Wraps the Connection Object Created by the ConnectionFactory to Add Object Pooling Functionality!
+		PoolableConnectionFactory pcf = new PoolableConnectionFactory(cf, gPool, null, null, false, true);
+		return new PoolingDataSource(gPool);
+	}
+
+	public GenericObjectPool getConnectionPool() {
+		return gPool;
+	}
+
+	// This Method Is Used To Print The Connection Pool Status
+	private void printDbStatus() {
+		System.out.println("Max.: " + getConnectionPool().getMaxActive() + "; Active: " + getConnectionPool().getNumActive() + "; Idle: " + getConnectionPool().getNumIdle());
+	}
+
+	public static void main(String[] args) {
+		ResultSet rsObj = null;
+		Connection connObj = null;
+		PreparedStatement pstmtObj = null;
+		ConnectionPool jdbcObj = new ConnectionPool();
+		try {
+			DataSource dataSource = jdbcObj.setUpPool();
+			jdbcObj.printDbStatus();
+
+			// Performing Database Operation!
+			System.out.println("\n=====Making A New Connection Object For Db Transaction=====\n");
+			connObj = dataSource.getConnection();
+			jdbcObj.printDbStatus();
+
+			pstmtObj = connObj.prepareStatement("SELECT * FROM ALUMNOS");
+			rsObj = pstmtObj.executeQuery();
+			while (rsObj.next()) {
+				System.out.println("Alumno Name: " + rsObj.getString("nombre"));
+			}
+			System.out.println("\n=====Releasing Connection Object To Pool=====\n");
+		} catch(Exception sqlException) {
+			sqlException.printStackTrace();
+		} finally {
+			try {
+				// Closing ResultSet Object
+				if(rsObj != null) {
+					rsObj.close();
+				}
+				// Closing PreparedStatement Object
+				if(pstmtObj != null) {
+					pstmtObj.close();
+				}
+				// Closing Connection Object
+				if(connObj != null) {
+					connObj.close();
+				}
+			} catch(Exception sqlException) {
+				sqlException.printStackTrace();
+			}
+		}
+		jdbcObj.printDbStatus();
+	}
+}
