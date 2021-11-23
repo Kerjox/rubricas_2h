e6413459d1f6cfa38d4bf83c5aa2a64e8058db9e
kerjox <kerjoxgamer095@gmail.com>
2021-11-23 13:25:41 +0100

    Added odoo module base examen

---
+++ SGE/odoo/addons/UBALDE_DANIEL-Examen-Odoo/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'UBALDE_DANIEL-Examen',
+    'author': "Kerjox095",
+    'license': "AGPL-3",
+    'version': '1.0',
+    'category': 'Examen',
+    'summary': '',
+    'description': 'Examen',
+    'website': '',
+    'depends': ['base'],
+    'data': ['data/views.xml'],
+    #'demo': ['demo.xml']
+}
