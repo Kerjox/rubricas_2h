34157bdfefb1813a5bfd6364c32f39297c69880c
kerjox <kerjoxgamer095@gmail.com>
2021-12-06 18:14:56 +0100

    Actualizado ejercicio a la version de spring más moderna

--- DDI/Bloque3 Rubrica 1- Spring basico(dual)/src/main/java/com/kerjox/SpringBasico/AppMain.java
+++ DDI/Bloque3 Rubrica 1- Spring basico(dual)/src/main/java/com/kerjox/SpringBasico/AppMain.java
@@ -1,22 +1,21 @@
 package com.kerjox.SpringBasico;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppMain {
 
-	private static BeanFactory beanFactoryObj;
+	private static ApplicationContext beanFactoryObj;
 
 	// This Method Is Used To Load The Spring Bean Configuration File And Return The 'BeanFactory' Object
-	public static BeanFactory getBeanFactory() {
-		beanFactoryObj = new XmlBeanFactory(new ClassPathResource("spring-beans.xml"));
+	public static ApplicationContext getAppContext() {
+		beanFactoryObj = new ClassPathXmlApplicationContext("spring-beans.xml");
 		return beanFactoryObj;
 	}
 
 	// This Is The Main Method And In Here We Are Retrieving The Spring Bean via 'getBean()' Method
 	public static void main(String[] args) {
-		HelloWorld helloObj = (HelloWorld) getBeanFactory().getBean("helloWorld");
+		HelloWorld helloObj = (HelloWorld) getAppContext().getBean("helloWorld");
 		System.out.println(helloObj.getUsername());
 	}
 }
