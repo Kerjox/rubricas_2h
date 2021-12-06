040894888600f8b083e8a2fcb10274aecc7febb3
kerjox <kerjoxgamer095@gmail.com>
2021-12-06 18:15:56 +0100

    Addeded SpringBasicoII

---
+++ DDI/Bloque3 Rubrica 2 - Spring basico II (dual)/src/main/java/AppMain.java
@@ -0,0 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppMain {
+
+	private static ApplicationContext getAppContext() {
+
+		return new ClassPathXmlApplicationContext("spring-beans.xml");
+	}
+
+	public static void main(String[] args) {
+
+		Department deptObj = (Department) getAppContext().getBean("department");
+		deptObj.showResult();
+	}
+}
---
+++ DDI/Bloque3 Rubrica 2 - Spring basico II (dual)/src/main/java/Department.java
@@ -0,0 +1,49 @@
+import java.util.Map;
+
+public class Department {
+
+	private int departmentId;
+	private String departmentName;
+	private Map<String, String> employee;
+
+	public Department() {
+	}
+
+	public Department(int dept_id, String dept_name, Map<String, String> dept_employee) {
+		this.departmentId = dept_id;
+		this.departmentName = dept_name;
+		this.employee = dept_employee;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+	public Map<String, String> getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Map<String, String> employee) {
+		this.employee = employee;
+	}
+
+	public void showResult() {
+		System.out.println("Department Details? Id= " + departmentId + ", Name= " + departmentName);
+		System.out.println("\nEmployee Details?\n");
+		for (Map.Entry<String, String> mapValue : employee.entrySet()) {
+			System.out.println("Designation= " + mapValue.getKey() + ", Name= " + mapValue.getValue());
+		}
+	}
+}
