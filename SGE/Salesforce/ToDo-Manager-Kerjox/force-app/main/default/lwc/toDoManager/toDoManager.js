728113ac22d2ee20744c607fc5c737389049b63d
kerjox <kerjoxgamer095@gmail.com>
2022-02-13 12:54:16 +0100

    Primera componenete completado

--- SGE/Salesforce/ToDo-Manager-Kerjox/force-app/main/default/lwc/toDoManager/toDoManager.js
+++ SGE/Salesforce/ToDo-Manager-Kerjox/force-app/main/default/lwc/toDoManager/toDoManager.js
@@ -1,3 +1,81 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class ToDoManager extends LightningElement {}
+export default class ToDoManager extends LightningElement {
+
+  @track time = "9:00 PM";
+  @track greeting = "Good Evening";
+  @track todos = [];
+
+  connectedCallback() {
+
+    this.getTime();
+
+    setInterval(() => {
+      this.getTime();
+      console.log("Set interval called");
+    }, 1000*60);
+  }
+
+  getTime() {
+
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    this.time = this.getDoubleDigit(this.getHour(hours)) + ":" + this.getDoubleDigit(minutes) + " " + this.getMidDay(hours);
+    
+    this.setGreeting(hours);
+  }
+
+  getHour(hour) {
+    return hour === 0 ? 12 : hour > 12 ? hour - 12 : hour;
+  }
+
+  getMidDay(hour) {
+    return hour < 12 ? "AM" : "PM";
+  }
+
+  getDoubleDigit(digit) {
+    return digit < 10 ? "0" + digit : digit;
+  }
+
+  setGreeting(hour) {
+
+    if (hour < 12) {
+      this.greeting = "Good Morning";
+    } else if (hour >= 12 && hour < 17) {
+      this.greeting = "Good Afternoon";
+    } else {
+      this.greeting = "Good Evening";
+    }
+  }
+
+  addTodoHandler() {
+
+    const inputBox = this.template.querySelector("lightning-input");
+    
+    const todo = {
+      todoId: this.todos.length,
+      todoName: inputBox.value,
+      done: false,
+      todoDate: new Date()
+    }
+    
+    this.todos.push(todo);
+    inputBox.value = "";
+  }
+
+  get upcomingTasks() {
+
+    return this.todos && this.todos.length 
+    ? this.todos.filter(todo => !todo.done) 
+    : [];
+  }
+
+  get completedTasks() {
+  
+    return this.todos && this.todos.length 
+    ? this.todos.filter(todo => todo.done) 
+    : [];
+  }
+}
