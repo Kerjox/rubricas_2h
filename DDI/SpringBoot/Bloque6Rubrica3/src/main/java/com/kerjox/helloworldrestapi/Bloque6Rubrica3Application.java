08c1201f4d16b1dcf917a585659ca5e7b4054df5
kerjox <kerjoxgamer095@gmail.com>
2022-02-18 09:12:20 +0100

    Bloque6Rubrica3

---
+++ DDI/SpringBoot/Bloque6Rubrica3/src/main/java/com/kerjox/helloworldrestapi/Bloque6Rubrica3Application.java
@@ -0,0 +1,15 @@
+package com.kerjox.helloworldrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.kerjox.helloworldrestapi.*")
+public class Bloque6Rubrica3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bloque6Rubrica3Application.class, args);
+	}
+
+}
---
+++ DDI/SpringBoot/Bloque6Rubrica3/src/test/java/com/kerjox/helloworldrestapi/Bloque6Rubrica3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerjox.helloworldrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bloque6Rubrica3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
