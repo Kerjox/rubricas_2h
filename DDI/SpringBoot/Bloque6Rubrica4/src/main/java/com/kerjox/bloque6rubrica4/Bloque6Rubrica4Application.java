a22452b2472a1be0ee35bcdf6927fedd3771263b
kerjox <kerjoxgamer095@gmail.com>
2022-02-25 09:43:59 +0100

    Added Bloque6Rubrica4

---
+++ DDI/SpringBoot/Bloque6Rubrica4/src/main/java/com/kerjox/bloque6rubrica4/Bloque6Rubrica4Application.java
@@ -0,0 +1,13 @@
+package com.kerjox.bloque6rubrica4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bloque6Rubrica4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bloque6Rubrica4Application.class, args);
+	}
+
+}
---
+++ DDI/SpringBoot/Bloque6Rubrica4/src/test/java/com/kerjox/bloque6rubrica4/Bloque6Rubrica4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerjox.bloque6rubrica4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bloque6Rubrica4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
